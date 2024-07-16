bca2a3ebacd763bb7c20464f1203aba49f493721
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-16 23:51:56 +0300

    exceptions

---
+++ weather/exeptions.py
@@ -0,0 +1,8 @@
+class CityNotFoundException(Exception):
+    """Custom exception for when a city is not found."""
+    pass
+
+
+class ErrorFetchingData(Exception):
+    """Error fetching data"""
+    pass
