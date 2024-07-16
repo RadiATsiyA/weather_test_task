76b968e6ea1db49ba6789df54440a8723c530a5b
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-16 23:54:46 +0300

    added view of main task

---
+++ weather/views.py
@@ -0,0 +1,25 @@
+import requests
+from django.shortcuts import render
+from django.views import View
+
+from .forms import CityForm
+from .service import get_weather_data
+
+
+class WeatherView(View):
+    form_class = CityForm
+    template_name = "weather/weather.html"
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        weather_data = None
+        if form.is_valid():
+            city = form.cleaned_data['city']
+            weather_data = get_weather_data(city)
+            print(weather_data)
+        return render(request, self.template_name, {'form': form, 'weather_data': weather_data})
+
