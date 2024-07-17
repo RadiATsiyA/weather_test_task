fc105961b00b16e7dfe2e3e53e883964c8508b07
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-17 23:49:01 +0300

    changed attributes ob input

--- weather/forms.py
+++ weather/forms.py
@@ -3,7 +3,8 @@ from django import forms
 
 class CityForm(forms.Form):
     city = forms.CharField(label="city", max_length=50, widget=forms.TextInput(attrs={
-        'class': 'form-class',
-        'placeholder': 'Выберите город',
-        'id': 'inputName',
+        'class': 'input',
+        'placeholder': 'Enter city...',
+        'aria-label': 'Enter city',
+        'aria-describedby': 'button-addon'
     }))
