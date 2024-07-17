d09687d5589aeac6504d964f8fd500ef14521318
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-17 23:49:55 +0300

    new buisnes logic functions

--- weather/service.py
+++ weather/service.py
@@ -1,7 +1,15 @@
+from datetime import datetime
 
 from django.conf import settings
 import requests
 from .exeptions import CityNotFoundException
+from .models import SearchHistory
+
+
+def update_search_history(session_key, city: str) -> None:
+    search_history, created = SearchHistory.objects.get_or_create(session_key=session_key, city=city)
+    search_history.search_count += 1
+    search_history.save()
 
 
 def get_coordinates_by_city(city: str) -> list[float, float]:
@@ -10,11 +18,12 @@ def get_coordinates_by_city(city: str) -> list[float, float]:
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
-    if data:
-        coordinates = [float(data['results'][0]['lat']), float(data['results'][0]['lon'])]
-        return coordinates
-    else:
+
+    if not data or not data.get('results'):
         raise CityNotFoundException(f"No data found for the city: {city}")
+
+    coordinates = [float(data['results'][0]['lat']), float(data['results'][0]['lon'])]
+    return coordinates
 
 
 def get_weather_data(city: str):
@@ -29,6 +38,19 @@ def get_weather_data(city: str):
 
     response = requests.get(api_url)
     response.raise_for_status()
-    print(response.json())
-    return response.json()
+    weather_data = response.json()
+    formatted_weather_data = format_weather_data(weather_data)
+    return formatted_weather_data
+
+
+def format_weather_data(weather_data):
+    weather_data['current']['time'] = format_date(weather_data['current']['time'])
+    weather_data['daily']['time'] = [format_date(date) for date in weather_data['daily']['time']]
+    return weather_data
+
+
+def format_date(date_str):
+    date_obj = datetime.fromisoformat(date_str.replace('T', ' '))
+    formatted_date = date_obj.strftime("%d.%m, %a")
+    return formatted_date
 
