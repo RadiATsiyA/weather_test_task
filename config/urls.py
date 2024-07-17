8ee1b354981c8f24f3b102d31b4e7f50f82088ee
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-17 23:48:25 +0300

    a little changes

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("weather.urls", namespace="weather")),
+    path("api/", include("api.urls", namespace="api")),
 ]
 
 if settings.DEBUG:
