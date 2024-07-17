0dde69f5ca35710b112d9d9846f95b0e459ffd76
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-17 23:49:22 +0300

    model for saving searchs

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class SearchHistory(models.Model):
+    session_key = models.CharField(max_length=256)
+    city = models.CharField(max_length=100)
+    search_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.city} - {self.search_count} times"
