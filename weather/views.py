3fd626f9e6f213f3ccfbe8d9492f1c3b7561cc2e
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-18 14:15:57 +0300

    added previus city suggestion

--- weather/views.py
+++ weather/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 from .forms import CityForm
-from .service import get_weather_data, update_search_history
+from .service import get_weather_data, update_search_history, get_session_key
 
 
 class WeatherView(View):
@@ -10,7 +10,9 @@ class WeatherView(View):
     template_name = "weather/weather.html"
 
     def get(self, request):
-        form = self.form_class()
+        last_city = request.session.get('last_city')
+        initial_data = {'city': last_city} if last_city else None
+        form = self.form_class(initial=initial_data)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
@@ -20,10 +22,8 @@ class WeatherView(View):
             city = form.cleaned_data['city']
             weather_data = get_weather_data(city)
 
-            session_key = request.session.session_key
-            if not session_key:
-                request.session.create()
-                session_key = request.session.session_key
+            session_key = get_session_key(request)
+            request.session['last_city'] = city
             update_search_history(session_key, city)
-        return render(request, self.template_name, {'form': form, 'weather_data': weather_data})
+        return render(request, self.template_name, {'form': form, 'weather_data': weather_data, 'city': city})
 
