906b723ec81482862b1938b4556ac953ab103563
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-18 14:11:17 +0300

    tests for serializer

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import TestCase, RequestFactory
+from rest_framework.test import APIClient
+from weather.models import SearchHistory
+from .serializers import SearchHistorySerializer
 
-# Create your tests here.
+
+class SearchHistorySerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.search_history_data = {
+            'city': 'New York',
+            'search_count': 5,
+        }
+
+        self.search_history = SearchHistory.objects.create(
+            city=self.search_history_data['city'],
+            search_count=self.search_history_data['search_count'],
+        )
+
+        self.serializer = SearchHistorySerializer(instance=self.search_history)
+
+    def test_serializer_contains_expected_fields(self):
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), {'city', 'search_count'})
+
+    def test_serializer_data(self):
+        data = self.serializer.data
+        self.assertEqual(data['city'], self.search_history_data['city'])
+        self.assertEqual(data['search_count'], self.search_history_data['search_count'])
+
+    def test_create_valid_serializer(self):
+        serializer_data = {
+            'city': 'Los Angeles',
+            'search_count': 3,
+        }
+        serializer = SearchHistorySerializer(data=serializer_data)
+        self.assertTrue(serializer.is_valid())
+        search_history_obj = serializer.save()
+        self.assertEqual(search_history_obj.city, serializer_data['city'])
+        self.assertEqual(search_history_obj.search_count, serializer_data['search_count'])
+
